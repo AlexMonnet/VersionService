0f4bd8c355dab049818786037afed0f7a71916a0
Alex Monnet <alex.monnet@e-signum.com>
2019-11-04 12:39:43 -0600

    First pass of version number comparison with green lights across the board.

--- version-number/src/main/java/com/monnet/versionnumber/VersionNumber.java
+++ version-number/src/main/java/com/monnet/versionnumber/VersionNumber.java
@@ -1,17 +1,22 @@
 package com.monnet.versionnumber;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
-public class VersionNumber implements Comparable<VersionNumber> {
+public class VersionNumber implements Comparable<VersionNumber> 
+{
 
     private String versionNumberString;
 
-    public VersionNumber(final String versionNumberString) {
+    public VersionNumber(final String versionNumberString) 
+    {
         this.versionNumberString = versionNumberString;
     }
 
-    public String getVersionNumberString(){
+    public String getVersionNumberString()
+    {
         return this.versionNumberString;
     }
 
@@ -20,28 +25,67 @@ public class VersionNumber implements Comparable<VersionNumber> {
      *  comparison can more easily be used for sorting and comparison.
      *
      * @param otherVersionNumber the target VersionNumber class to compare against
-     * @return int   1 - The source version is greater than the target version
+     * @return Integer   1 - The source version is greater than the target version
      *               0 - The source version is equal to the target version
      *              -1 - The source version is less than the target version
      */
     @Override
-    public int compareTo(VersionNumber otherVersionNumber){ 
+    public int compareTo(VersionNumber otherVersionNumber)
+    { 
         int comparisonValue = 0;
         final String sourceVersionNumberString = this.versionNumberString;
         final String targetVersionNumberString = otherVersionNumber.getVersionNumberString();
 
-        if(sourceVersionNumberString.equals(targetVersionNumberString)){
-            comparisonValue = 0;
-        } else {
-            final List<String> sourceSplitVersionList = Arrays.asList(this.versionNumberString.split("."));
-            final List<String> targetSplitVersionList = Arrays.asList(this.versionNumberString.split("."));
+        if(!sourceVersionNumberString.equals(targetVersionNumberString))
+        {
+            final List<String> sourceSplitVersionList = removeTrailingZeroVersionIdentifiers(Arrays.asList(sourceVersionNumberString.split("\\.")));
+            final List<String> targetSplitVersionList = removeTrailingZeroVersionIdentifiers(Arrays.asList(targetVersionNumberString.split("\\.")));
+
             final int minNumberOfDots = Math.min(sourceSplitVersionList.size(), targetSplitVersionList.size());
 
             for(int i = 0; i < minNumberOfDots; i++){
-                
+                Integer sourceNumber = Integer.parseInt(sourceSplitVersionList.get(i));
+                Integer targetNumber = Integer.parseInt(targetSplitVersionList.get(i));
+                if(sourceNumber > targetNumber)
+                {
+                    comparisonValue = 1;
+                    break;
+                } 
+                else if (sourceNumber < targetNumber)
+                {
+                    comparisonValue = -1;
+                    break;
+                } 
+            }
+
+            int sourceSplitSize = sourceSplitVersionList.size();
+            int targetSplitSize = targetSplitVersionList.size();
+            if(comparisonValue == 0 && sourceSplitSize != targetSplitSize){
+                comparisonValue = sourceSplitSize > targetSplitSize ? 1 : -1;
             }
         }
 
         return comparisonValue;
+    }
+
+    private List<String> removeTrailingZeroVersionIdentifiers(List<String> splitVersionStringList)
+    {
+        final List<String> cleanedSplitVersionStringList = new ArrayList<>(splitVersionStringList);
+        final List<String> reversedSplitVersionStringList = new ArrayList<>(splitVersionStringList);
+        Collections.reverse(reversedSplitVersionStringList);
+        for (String versionIdenfierString : reversedSplitVersionStringList) 
+        {
+            Integer versionIdenfier = Integer.parseInt(versionIdenfierString);
+            if(versionIdenfier == 0)
+            {
+                cleanedSplitVersionStringList.remove(versionIdenfierString);
+            } 
+            else
+            {
+                break;
+            }
+        }
+        
+        return cleanedSplitVersionStringList;
     }
 }
