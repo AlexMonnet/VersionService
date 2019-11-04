d6879030e0ff9da8860b7c01b2b2dbaa2168005f
Alex Monnet <alex.monnet@e-signum.com>
2019-11-03 23:17:10 -0600

    Added more tests for more-complete case testing.

--- version-number/src/test/java/com/monnet/versionnumber/VersionNumberTest.java
+++ version-number/src/test/java/com/monnet/versionnumber/VersionNumberTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.*;
 
 public class VersionNumberTest {
     @Test
-    public void testVersionNumber_compareTo_lessThan(){
+    public void testVersionNumber_compareTo_lessThan_equalVersionIdentifierFields(){
         final VersionNumber sourceVersionNumber = new VersionNumber("2.1");
         final VersionNumber targetVersionNumber = new VersionNumber("2.2");
         final int versionNumberComparison = sourceVersionNumber.compareTo(targetVersionNumber);
@@ -13,7 +13,7 @@ public class VersionNumberTest {
     }
 
     @Test
-    public void testVersionNumber_compareTo_greaterThan(){
+    public void testVersionNumber_compareTo_greaterThan_equalVersionIdentifierFields(){
         final VersionNumber sourceVersionNumber = new VersionNumber("2.2");
         final VersionNumber targetVersionNumber = new VersionNumber("2.1");
         final int versionNumberComparison = sourceVersionNumber.compareTo(targetVersionNumber);
@@ -21,9 +21,57 @@ public class VersionNumberTest {
     }
 
     @Test
-    public void testVersionNumber_compareTo_equalTo(){
+    public void testVersionNumber_compareTo_equalTo_equalVersionIdentifierFields(){
         final VersionNumber sourceVersionNumber = new VersionNumber("2.2");
         final VersionNumber targetVersionNumber = new VersionNumber("2.2");
+        final int versionNumberComparison = sourceVersionNumber.compareTo(targetVersionNumber);
+        Assert.assertEquals(0, versionNumberComparison);
+    }
+
+    @Test
+    public void testVersionNumber_compareTo_lessThan_moreSourceVersionIdentifierFields(){
+        final VersionNumber sourceVersionNumber = new VersionNumber("2.1.1");
+        final VersionNumber targetVersionNumber = new VersionNumber("2.2");
+        final int versionNumberComparison = sourceVersionNumber.compareTo(targetVersionNumber);
+        Assert.assertEquals(-1, versionNumberComparison);
+    }
+
+    @Test
+    public void testVersionNumber_compareTo_greaterThan_moreSourceVersionIdentifierFields(){
+        final VersionNumber sourceVersionNumber = new VersionNumber("2.2.2");
+        final VersionNumber targetVersionNumber = new VersionNumber("2.1");
+        final int versionNumberComparison = sourceVersionNumber.compareTo(targetVersionNumber);
+        Assert.assertEquals(1, versionNumberComparison);
+    }
+
+    @Test
+    public void testVersionNumber_compareTo_equalTo_moreSourceVersionIdentifierFields(){
+        final VersionNumber sourceVersionNumber = new VersionNumber("2.2.0");
+        final VersionNumber targetVersionNumber = new VersionNumber("2.2");
+        final int versionNumberComparison = sourceVersionNumber.compareTo(targetVersionNumber);
+        Assert.assertEquals(0, versionNumberComparison);
+    }
+
+    @Test
+    public void testVersionNumber_compareTo_lessThan_lessSourceVersionIdentifierFields(){
+        final VersionNumber sourceVersionNumber = new VersionNumber("2.1");
+        final VersionNumber targetVersionNumber = new VersionNumber("2.2.2");
+        final int versionNumberComparison = sourceVersionNumber.compareTo(targetVersionNumber);
+        Assert.assertEquals(-1, versionNumberComparison);
+    }
+
+    @Test
+    public void testVersionNumber_compareTo_greaterThan_lessSourceVersionIdentifierFields(){
+        final VersionNumber sourceVersionNumber = new VersionNumber("2.2");
+        final VersionNumber targetVersionNumber = new VersionNumber("2.1.1");
+        final int versionNumberComparison = sourceVersionNumber.compareTo(targetVersionNumber);
+        Assert.assertEquals(1, versionNumberComparison);
+    }
+
+    @Test
+    public void testVersionNumber_compareTo_equalTo_lessSourceVersionIdentifierFields(){
+        final VersionNumber sourceVersionNumber = new VersionNumber("2.2");
+        final VersionNumber targetVersionNumber = new VersionNumber("2.2.0");
         final int versionNumberComparison = sourceVersionNumber.compareTo(targetVersionNumber);
         Assert.assertEquals(0, versionNumberComparison);
     }
