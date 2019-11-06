9c7b8c7a50822f298ee8f77bb82f6e52a665037e
Alex Monnet <alex.monnet@e-signum.com>
2019-11-06 17:50:15 -0600

    Have a basic web-based service for the version comparing.

--- web-service/src/main/java/com/monnet/webservice/Application.java
+++ web-service/src/main/java/com/monnet/webservice/Application.java
@@ -1,10 +1,27 @@
 package com.monnet.webservice;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class Application {
-    public static void main(String [ ] args)
-    {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping(value = "/")
+    public String index() {
+        return "Version Service is live. Use /api/compareVersions?sourceVersion=<xxx>&targetVersion=<xxx> to use the service";
+    }
+
+    @RequestMapping("/api/compareVersions")
+    public String compareValues(@RequestParam String sourceVersion, @RequestParam String targetVersion) {
         WebService service = new WebService();
-        String comparisonString = service.compareVersions("1.2", "2.3");
-        System.out.println(comparisonString);
+        String comparisonString = service.compareVersions(sourceVersion, targetVersion);
+        return comparisonString;
     }
 }
