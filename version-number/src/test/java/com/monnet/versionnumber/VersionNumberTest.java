9fecb7b04499e285c1342da7d6c6ffc4b4d1483f
Alex Monnet <alex.monnet@e-signum.com>
2019-11-06 19:02:07 -0600

    Fixed some minor formatting

--- version-number/src/test/java/com/monnet/versionnumber/VersionNumberTest.java
+++ version-number/src/test/java/com/monnet/versionnumber/VersionNumberTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.*;
 
 public class VersionNumberTest {
     @Test
-    public void testVersionNumber_compareTo_lessThan_equalVersionIdentifierFields(){
+    public void testVersionNumber_compareTo_lessThan_equalVersionIdentifierFields() {
         final VersionNumber sourceVersionNumber = new VersionNumber("2.1");
         final VersionNumber targetVersionNumber = new VersionNumber("2.2");
         final int versionNumberComparison = sourceVersionNumber.compareTo(targetVersionNumber);
@@ -13,7 +13,7 @@ public class VersionNumberTest {
     }
 
     @Test
-    public void testVersionNumber_compareTo_greaterThan_equalVersionIdentifierFields(){
+    public void testVersionNumber_compareTo_greaterThan_equalVersionIdentifierFields() {
         final VersionNumber sourceVersionNumber = new VersionNumber("2.2");
         final VersionNumber targetVersionNumber = new VersionNumber("2.1");
         final int versionNumberComparison = sourceVersionNumber.compareTo(targetVersionNumber);
@@ -21,7 +21,7 @@ public class VersionNumberTest {
     }
 
     @Test
-    public void testVersionNumber_compareTo_equalTo_equalVersionIdentifierFields(){
+    public void testVersionNumber_compareTo_equalTo_equalVersionIdentifierFields() {
         final VersionNumber sourceVersionNumber = new VersionNumber("2.2");
         final VersionNumber targetVersionNumber = new VersionNumber("2.2");
         final int versionNumberComparison = sourceVersionNumber.compareTo(targetVersionNumber);
@@ -29,7 +29,7 @@ public class VersionNumberTest {
     }
 
     @Test
-    public void testVersionNumber_compareTo_lessThan_moreSourceVersionIdentifierFields(){
+    public void testVersionNumber_compareTo_lessThan_moreSourceVersionIdentifierFields() {
         final VersionNumber sourceVersionNumber = new VersionNumber("2.1.1");
         final VersionNumber targetVersionNumber = new VersionNumber("2.2");
         final int versionNumberComparison = sourceVersionNumber.compareTo(targetVersionNumber);
@@ -37,7 +37,7 @@ public class VersionNumberTest {
     }
 
     @Test
-    public void testVersionNumber_compareTo_greaterThan_moreSourceVersionIdentifierFields(){
+    public void testVersionNumber_compareTo_greaterThan_moreSourceVersionIdentifierFields() {
         final VersionNumber sourceVersionNumber = new VersionNumber("2.2.2");
         final VersionNumber targetVersionNumber = new VersionNumber("2.1");
         final int versionNumberComparison = sourceVersionNumber.compareTo(targetVersionNumber);
@@ -45,7 +45,7 @@ public class VersionNumberTest {
     }
 
     @Test
-    public void testVersionNumber_compareTo_equalTo_moreSourceVersionIdentifierFields(){
+    public void testVersionNumber_compareTo_equalTo_moreSourceVersionIdentifierFields() {
         final VersionNumber sourceVersionNumber = new VersionNumber("2.2.0");
         final VersionNumber targetVersionNumber = new VersionNumber("2.2");
         final int versionNumberComparison = sourceVersionNumber.compareTo(targetVersionNumber);
@@ -53,7 +53,7 @@ public class VersionNumberTest {
     }
 
     @Test
-    public void testVersionNumber_compareTo_lessThan_lessSourceVersionIdentifierFields(){
+    public void testVersionNumber_compareTo_lessThan_lessSourceVersionIdentifierFields() {
         final VersionNumber sourceVersionNumber = new VersionNumber("2.1");
         final VersionNumber targetVersionNumber = new VersionNumber("2.2.2");
         final int versionNumberComparison = sourceVersionNumber.compareTo(targetVersionNumber);
@@ -61,7 +61,7 @@ public class VersionNumberTest {
     }
 
     @Test
-    public void testVersionNumber_compareTo_greaterThan_lessSourceVersionIdentifierFields(){
+    public void testVersionNumber_compareTo_greaterThan_lessSourceVersionIdentifierFields() {
         final VersionNumber sourceVersionNumber = new VersionNumber("2.2");
         final VersionNumber targetVersionNumber = new VersionNumber("2.1.1");
         final int versionNumberComparison = sourceVersionNumber.compareTo(targetVersionNumber);
@@ -69,7 +69,7 @@ public class VersionNumberTest {
     }
 
     @Test
-    public void testVersionNumber_compareTo_equalTo_lessSourceVersionIdentifierFields(){
+    public void testVersionNumber_compareTo_equalTo_lessSourceVersionIdentifierFields() {
         final VersionNumber sourceVersionNumber = new VersionNumber("2.2");
         final VersionNumber targetVersionNumber = new VersionNumber("2.2.0");
         final int versionNumberComparison = sourceVersionNumber.compareTo(targetVersionNumber);
@@ -77,7 +77,7 @@ public class VersionNumberTest {
     }
 
     @Test
-    public void testVersionNumber_compareTo_lessThan_extraIntermediateSourceVersionIdentifierFields(){
+    public void testVersionNumber_compareTo_lessThan_extraIntermediateSourceVersionIdentifierFields() {
         final VersionNumber sourceVersionNumber = new VersionNumber("2.0001.0");
         final VersionNumber targetVersionNumber = new VersionNumber("2.2.2");
         final int versionNumberComparison = sourceVersionNumber.compareTo(targetVersionNumber);
@@ -85,7 +85,7 @@ public class VersionNumberTest {
     }
 
     @Test
-    public void testVersionNumber_compareTo_greaterThan_extraIntermediateSourceVersionIdentifierFields(){
+    public void testVersionNumber_compareTo_greaterThan_extraIntermediateSourceVersionIdentifierFields() {
         final VersionNumber sourceVersionNumber = new VersionNumber("2.2.000000.2");
         final VersionNumber targetVersionNumber = new VersionNumber("2.2.0.1");
         final int versionNumberComparison = sourceVersionNumber.compareTo(targetVersionNumber);
@@ -93,7 +93,7 @@ public class VersionNumberTest {
     }
 
     @Test
-    public void testVersionNumber_compareTo_equalTo_extraIntermediateSourceVersionIdentifierFields(){
+    public void testVersionNumber_compareTo_equalTo_extraIntermediateSourceVersionIdentifierFields() {
         final VersionNumber sourceVersionNumber = new VersionNumber("2.0002.00000000");
         final VersionNumber targetVersionNumber = new VersionNumber("2.2.0");
         final int versionNumberComparison = sourceVersionNumber.compareTo(targetVersionNumber);
@@ -101,7 +101,7 @@ public class VersionNumberTest {
     }
 
     @Test
-    public void testVersionNumber_compareTo_lessThan_extraIntermediateTargetVersionIdentifierFields(){
+    public void testVersionNumber_compareTo_lessThan_extraIntermediateTargetVersionIdentifierFields() {
         final VersionNumber sourceVersionNumber = new VersionNumber("2.1.0");
         final VersionNumber targetVersionNumber = new VersionNumber("2.0001.0000.2");
         final int versionNumberComparison = sourceVersionNumber.compareTo(targetVersionNumber);
@@ -109,7 +109,7 @@ public class VersionNumberTest {
     }
 
     @Test
-    public void testVersionNumber_compareTo_greaterThan_extraIntermediateTargetVersionIdentifierFields(){
+    public void testVersionNumber_compareTo_greaterThan_extraIntermediateTargetVersionIdentifierFields() {
         final VersionNumber sourceVersionNumber = new VersionNumber("2.2.0.2");
         final VersionNumber targetVersionNumber = new VersionNumber("2.0002.00000.1");
         final int versionNumberComparison = sourceVersionNumber.compareTo(targetVersionNumber);
@@ -117,7 +117,7 @@ public class VersionNumberTest {
     }
 
     @Test
-    public void testVersionNumber_compareTo_equalTo_extraIntermediateTargetVersionIdentifierFields(){
+    public void testVersionNumber_compareTo_equalTo_extraIntermediateTargetVersionIdentifierFields() {
         final VersionNumber sourceVersionNumber = new VersionNumber("2.2.0");
         final VersionNumber targetVersionNumber = new VersionNumber("2.0002.000000000");
         final int versionNumberComparison = sourceVersionNumber.compareTo(targetVersionNumber);
